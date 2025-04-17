4f8087047f8da805424fe7ef95f20d6de5fbb8ef
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-17 09:52:08 +0700

    cek1

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -3,8 +3,45 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
-class sub_category extends Model
+class SubCategory extends Model
 {
-    //
+    use SoftDeletes, LogsActivity;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'slug',
+    ];
+
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        // Jika nilai slug tidak diberikan, generate slug dari nm_produk
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name']);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- config/livewire.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'url' => env('APP_URL', 'https://gprb-production.up.railway.app'),
-    'asset_url' => env('ASSET_URL', 'https://gprb-production.up.railway.app'),
-];
